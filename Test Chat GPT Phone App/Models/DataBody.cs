f3ad1081929b2dbefb795c72d193b3af59127a6f
Maxington20 <max.herrington@gmail.com>
2023-04-17 15:34:36 -0400

    Add project files.

---
+++ Test Chat GPT Phone App/Models/DataBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Chat_GPT_Phone_App.Models
+{
+    public class DataBody
+    {
+        public string model { get; set; }
+        public List<Message> messages { get; set; }
+    }
+}
---
+++ Test Chat GPT Phone App/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Chat_GPT_Phone_App.Models
+{
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+}
